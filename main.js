68662455d0dfe96433517bffbfcdde229550b82a
andrey rodriguez <andreyrodcha757@gmail.com>
2022-09-05 16:09:31 -0600

    Proyecto inicial

---
+++ main.js
@@ -0,0 +1,470 @@
+import './style.css'
+import * as THREE from 'https://cdn.skypack.dev/three@0.136';
+
+import { FirstPersonControls } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/FirstPersonControls.js';
+
+
+const KEYS = {
+  'a': 65,
+  's': 83,
+  'w': 87,
+  'd': 68,
+};
+
+function clamp(x, a, b) {
+  return Math.min(Math.max(x, a), b);
+}
+
+class InputController {
+  constructor(target) {
+    this.target_ = target || document;
+    this.initialize_();
+  }
+
+  initialize_() {
+    this.current_ = {
+      leftButton: false,
+      rightButton: false,
+      mouseXDelta: 0,
+      mouseYDelta: 0,
+      mouseX: 0,
+      mouseY: 0,
+    };
+    this.previous_ = null;
+    this.keys_ = {};
+    this.previousKeys_ = {};
+    this.target_.addEventListener('mousedown', (e) => this.onMouseDown_(e), false);
+    this.target_.addEventListener('mousemove', (e) => this.onMouseMove_(e), false);
+    this.target_.addEventListener('mouseup', (e) => this.onMouseUp_(e), false);
+    this.target_.addEventListener('keydown', (e) => this.onKeyDown_(e), false);
+    this.target_.addEventListener('keyup', (e) => this.onKeyUp_(e), false);
+  }
+
+  onMouseMove_(e) {
+    this.current_.mouseX = e.pageX - window.innerWidth / 2;
+    this.current_.mouseY = e.pageY - window.innerHeight / 2;
+
+    if (this.previous_ === null) {
+      this.previous_ = { ...this.current_ };
+    }
+
+    this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
+    this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+  }
+
+  onMouseDown_(e) {
+    this.onMouseMove_(e);
+
+    switch (e.button) {
+      case 0: {
+        this.current_.leftButton = true;
+        break;
+      }
+      case 2: {
+        this.current_.rightButton = true;
+        break;
+      }
+    }
+  }
+
+  onMouseUp_(e) {
+    this.onMouseMove_(e);
+
+    switch (e.button) {
+      case 0: {
+        this.current_.leftButton = false;
+        break;
+      }
+      case 2: {
+        this.current_.rightButton = false;
+        break;
+      }
+    }
+  }
+
+  onKeyDown_(e) {
+    this.keys_[e.keyCode] = true;
+  }
+
+  onKeyUp_(e) {
+    this.keys_[e.keyCode] = false;
+  }
+
+  key(keyCode) {
+    return !!this.keys_[keyCode];
+  }
+
+  isReady() {
+    return this.previous_ !== null;
+  }
+
+  update(_) {
+    if (this.previous_ !== null) {
+      this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
+      this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+
+      this.previous_ = { ...this.current_ };
+    }
+  }
+};
+
+
+class FirstPersonCamera {
+  constructor(camera, objects) {
+    this.camera_ = camera;
+    this.input_ = new InputController();
+    this.rotation_ = new THREE.Quaternion();
+    this.translation_ = new THREE.Vector3(0, 5, 0);
+    this.phi_ = 0;
+    this.phiSpeed_ = 30;
+    this.theta_ = 0;
+    this.thetaSpeed_ = 5;
+    this.headBobActive_ = false;
+    this.headBobTimer_ = 0;
+    this.objects_ = objects;
+  }
+
+  update(timeElapsedS) {
+    this.updateRotation_(timeElapsedS);
+    this.updateCamera_(timeElapsedS);
+    this.updateTranslation_(timeElapsedS);
+    this.updateHeadBob_(timeElapsedS);
+    this.input_.update(timeElapsedS);
+  }
+
+  updateCamera_(_) {
+    this.camera_.quaternion.copy(this.rotation_);
+    this.camera_.position.copy(this.translation_);
+    this.camera_.position.y += Math.sin(this.headBobTimer_ * 3) * 0.2;
+
+    const forward = new THREE.Vector3(0, 0, -1);
+    forward.applyQuaternion(this.rotation_);
+
+    const dir = forward.clone();
+
+    forward.multiplyScalar(100);
+    forward.add(this.translation_);
+
+    let closest = forward;
+    const result = new THREE.Vector3();
+    const ray = new THREE.Ray(this.translation_, dir);
+    for (let i = 0; i < this.objects_.length; ++i) {
+      if (ray.intersectBox(this.objects_[i], result)) {
+        if (result.distanceTo(ray.origin) < closest.distanceTo(ray.origin)) {
+          closest = result.clone();
+        }
+      }
+    }
+
+    this.camera_.lookAt(closest);
+  }
+
+  updateHeadBob_(timeElapsedS) {
+    if (this.headBobActive_) {
+      const wavelength = Math.PI;
+      const nextStep = 1 + Math.floor(((this.headBobTimer_ + 0.0000015) * 10) / wavelength);
+      const nextStepTime = nextStep * wavelength / 10;
+      this.headBobTimer_ = Math.min(this.headBobTimer_ + timeElapsedS, nextStepTime);
+
+      if (this.headBobTimer_ == nextStepTime) {
+        this.headBobActive_ = false;
+      }
+    }
+  }
+  //////////////////////
+  updateTranslation_(timeElapsedS) {
+    const forwardVelocity = (this.input_.key(KEYS.w) ? 1 : 0) + (this.input_.key(KEYS.s) ? -1 : 0)
+    const strafeVelocity = (this.input_.key(KEYS.a) ? 1 : 0) + (this.input_.key(KEYS.d) ? -1 : 0)
+
+    const qx = new THREE.Quaternion();
+    qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
+
+    const forward = new THREE.Vector3(0, 0, -1);
+    forward.applyQuaternion(qx);
+    forward.multiplyScalar(forwardVelocity * timeElapsedS * 10);
+
+    const left = new THREE.Vector3(-1, 0, 0);
+    left.applyQuaternion(qx);
+    left.multiplyScalar(strafeVelocity * timeElapsedS * 10);
+
+    this.translation_.add(forward);
+    this.translation_.add(left);
+
+    if (forwardVelocity != 0 || strafeVelocity != 0) {
+      this.headBobActive_ = true;
+    }
+  }
+
+  updateRotation_(timeElapsedS) {
+    const xh = this.input_.current_.mouseXDelta / window.innerWidth;
+    const yh = this.input_.current_.mouseYDelta / window.innerHeight;
+
+    this.phi_ += -xh * this.phiSpeed_;
+    this.theta_ = clamp(this.theta_ + -yh * this.thetaSpeed_, -Math.PI / 3, Math.PI / 3);
+
+    const qx = new THREE.Quaternion();
+    qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
+    const qz = new THREE.Quaternion();
+    qz.setFromAxisAngle(new THREE.Vector3(1, 0, 0), this.theta_);
+
+    const q = new THREE.Quaternion();
+    q.multiply(qx);
+    q.multiply(qz);
+
+    this.rotation_.copy(q);
+  }
+}
+
+
+class FirstPersonCameraDemo {
+  constructor() {
+    this.initialize_();
+  }
+
+  initialize_() {
+    this.initializeRenderer_();
+    this.initializeLights_();
+    this.initializeScene_();
+    this.initializeDemo_();
+
+    this.previousRAF_ = null;
+    this.raf_();
+    this.onWindowResize_();
+  }
+
+  initializeDemo_() {
+    // this.controls_ = new FirstPersonControls(
+    //     this.camera_, this.threejs_.domElement);
+    // this.controls_.lookSpeed = 0.8;
+    // this.controls_.movementSpeed = 5;
+
+    this.fpsCamera_ = new FirstPersonCamera(this.camera_, this.objects_);
+  }
+
+  initializeRenderer_() {
+    this.threejs_ = new THREE.WebGLRenderer({
+      antialias: false,
+    });
+    this.threejs_.shadowMap.enabled = true;
+    this.threejs_.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.threejs_.setPixelRatio(window.devicePixelRatio);
+    this.threejs_.setSize(window.innerWidth, window.innerHeight);
+    this.threejs_.physicallyCorrectLights = true;
+    this.threejs_.outputEncoding = THREE.sRGBEncoding;
+
+    document.body.appendChild(this.threejs_.domElement);
+
+    window.addEventListener('resize', () => {
+      this.onWindowResize_();
+    }, false);
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 1000.0;
+    this.camera_ = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera_.position.set(0, 2, 0);
+
+    this.scene_ = new THREE.Scene();
+
+    this.uiCamera_ = new THREE.OrthographicCamera(
+      -1, 1, 1 * aspect, -1 * aspect, 1, 1000);
+    this.uiScene_ = new THREE.Scene();
+  }
+
+  initializeScene_() {
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      './textures/tears_ft.jpg',
+      './textures/tears_bk.jpg',
+      './textures/tears_up.jpg',
+      './textures/tears_dn.jpg',
+      './textures/tears_rt.jpg',
+      './textures/tears_lf.jpg',
+    ]);
+
+    texture.encoding = THREE.sRGBEncoding;
+    this.scene_.background = texture;
+
+    const mapLoader = new THREE.TextureLoader();
+    const maxAnisotropy = this.threejs_.capabilities.getMaxAnisotropy();
+    const checkerboard = mapLoader.load('./textures/checkerboard2.jpg');
+    checkerboard.anisotropy = maxAnisotropy;
+    checkerboard.wrapS = THREE.RepeatWrapping;
+    checkerboard.wrapT = THREE.RepeatWrapping;
+    checkerboard.repeat.set(10, 10);
+    checkerboard.encoding = THREE.sRGBEncoding;
+
+    const plane = new THREE.Mesh(
+      new THREE.PlaneGeometry(100, 100),
+      new THREE.MeshStandardMaterial({ map: checkerboard }));
+    plane.castShadow = true;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    this.scene_.add(plane);
+
+
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 4, 4),
+      this.loadMaterial_('box'));
+    box.position.set(4, 2, 2);
+    this.scene_.add(box);
+
+    const box2 = new THREE.Mesh(
+      new THREE.Vector3(5, 5, 5),
+      this.loadMaterial_('box'));
+    box2.position.set(4, 20, 2);
+    //this.scene_.add(box2);
+
+    const sphere = new THREE.Mesh(
+      new THREE.SphereGeometry(2, 100, 100),
+      this.loadMaterial_('box'));
+    sphere.position.set(16, 4, 10);
+    this.scene_.add(sphere);
+
+    console.log(box.setSize);
+    const concreteMaterial = this.loadMaterial_('muro', 4);
+
+    const wall1 = new THREE.Mesh(
+      new THREE.BoxGeometry(100, 15, 4),
+      concreteMaterial);
+    wall1.position.set(0, 7, -50);
+    this.scene_.add(wall1);
+
+    const wall2 = new THREE.Mesh(
+      new THREE.BoxGeometry(100, 15, 4),
+      concreteMaterial);
+    wall2.position.set(0, 7, 50);
+    this.scene_.add(wall2);
+
+    const wall3 = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 15, 100),
+      concreteMaterial);
+    wall3.position.set(50, 7, 0);
+    this.scene_.add(wall3);
+
+    const wall4 = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 15, 100),
+      concreteMaterial);
+    wall4.position.set(-50, 7, 0);
+    this.scene_.add(wall4);
+
+    // Create Box3 for each mesh in the scene so that we can
+    // do some easy intersection tests.
+    const meshes = [
+      plane, box, wall1, wall2, wall3, wall4];
+
+    this.objects_ = [];
+
+    for (let i = 0; i < meshes.length; ++i) {
+      const b = new THREE.Box3();
+      b.setFromObject(meshes[i]);
+      this.objects_.push();
+
+    }
+
+
+  }
+
+  initializeLights_() {
+    const distance = 100.0;
+    const angle = Math.PI / 2.0;
+    const penumbra = 0.5;
+    const decay = 1.0;
+
+    let light = new THREE.SpotLight(
+      0xFFFFFF, 200.0, distance, angle, penumbra, decay);
+    light.castShadow = true;
+    light.shadow.bias = -0.00001;
+    light.shadow.mapSize.width = 4096;
+    light.shadow.mapSize.height = 4096;
+    light.shadow.camera.near = 1;
+    light.shadow.camera.far = 100;
+
+    light.position.set(25, 25, 0);
+    light.lookAt(0, 0, 0);
+    this.scene_.add(light);
+
+    const upColour = 0xFFFF80;
+    const downColour = 0x808080;
+    light = new THREE.HemisphereLight(upColour, downColour, 0.5);
+    light.color.setHSL(0.6, 1, 0.6);
+    light.groundColor.setHSL(0.095, 1, 0.75);
+    light.position.set(0, 4, 0);
+    this.scene_.add(light);
+  }
+
+  loadMaterial_(name) {
+    const mapLoader = new THREE.TextureLoader();
+    const maxAnisotropy = this.threejs_.capabilities.getMaxAnisotropy();
+
+    let albedo;
+    if (name == 'box') {
+      albedo = mapLoader.load('./textures/checkerboard.png');
+      albedo.repeat.set(1, 1);
+    }
+    else {
+      albedo = mapLoader.load('./textures/arbusto.webp');
+      albedo.repeat.set(1.5, 1.5);
+    }
+    albedo.anisotropy = maxAnisotropy;
+    albedo.wrapS = THREE.RepeatWrapping;
+    albedo.wrapT = THREE.RepeatWrapping;
+    albedo.encoding = THREE.sRGBEncoding;
+    const material = new THREE.MeshStandardMaterial({
+      map: albedo,
+    });
+
+    return material;
+  }
+
+  onWindowResize_() {
+    let element = document.querySelector("#bg");
+    let width = element.offsetWidth;
+    let height = element.offsetHeight;
+
+    this.camera_.aspect = window.innerWidth / window.innerHeight;
+    this.camera_.updateProjectionMatrix();
+
+    this.uiCamera_.left = -this.camera_.aspect;
+    this.uiCamera_.right = this.camera_.aspect;
+    this.uiCamera_.updateProjectionMatrix();
+
+    this.threejs_.setSize(width, height);
+  }
+
+  raf_() {
+    requestAnimationFrame((t) => {
+      if (this.previousRAF_ === null) {
+        this.previousRAF_ = t;
+      }
+
+      this.step_(t - this.previousRAF_);
+      this.threejs_.autoClear = true;
+      this.threejs_.render(this.scene_, this.camera_);
+      this.threejs_.autoClear = false;
+      this.threejs_.render(this.uiScene_, this.uiCamera_);
+      this.previousRAF_ = t;
+      this.raf_();
+    });
+  }
+
+  step_(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.003;
+
+    // this.controls_.update(timeElapsedS);
+    this.fpsCamera_.update(timeElapsedS);
+  }
+}
+
+
+let _APP = null;
+let APP_ = null;
+window.addEventListener('DOMContentLoaded', async () => {
+  _APP = new FirstPersonCameraDemo();
+  Ammo().then((lib) => {
+    Ammo = lib;
+    APP_ = new BasicWorldDemo();
+    APP_.initialize();
+  });
+});
